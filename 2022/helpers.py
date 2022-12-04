ab81499f6542f44088275175c3e966e895baab01
Skaft <ragnar.gerholm@gmail.com>
2022-12-04 13:51:58 +0100

    clarify docstrings on AoCSolution

--- 2022/helpers.py
+++ 2022/helpers.py
@@ -9,6 +9,10 @@ class AoCSolution:
     Usage:
     Subclass this and override the clean_input() and main() methods.
     Then use the RUN method, not the main() method, to start.
+
+    Note:
+    The inputs/dayX.txt file should use the line "___INPUTSEP___"
+    to separate different input sets.
     """
     def __init__(self, day: int):
         input_file = f"inputs/day{day}.txt"
@@ -20,6 +24,14 @@ class AoCSolution:
         self.test_inputs = [self.clean_input(test) for test in self.raw_test_inputs]
 
     def run(self, input_select: int=1):
+        """
+        Run solution on the specified input.
+
+        input_select=0: Use the "true" input for the problem
+        input_select=1: Use the first set of test data,
+        input_select=2: Use the second set of test data,
+        ...
+        """
         if input_select == 0:
             cleaned_input = self.true_input
         else:
@@ -29,7 +41,7 @@ class AoCSolution:
 
     def clean_input(self, raw_input):
         """
-        Convert the puzzle input into a form useful for the problem
+        Converts the puzzle input into a form useful for the problem.
 
         (override this)
         """
@@ -37,7 +49,7 @@ class AoCSolution:
 
     def main(self, cleaned_input):
         """
-        The step sequence from pruned input to solution
+        Implements the step sequence from cleaned input to solution.
 
         (override this)
         """
