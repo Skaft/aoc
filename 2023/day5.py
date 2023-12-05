df64d6f8baf593c4e876473f8fa62eafc551baa2
Skaft <ragnar.gerholm@gmail.com>
2023-12-05 07:52:53 +0100

    tidy up day5

--- 2023/day5.py
+++ 2023/day5.py
@@ -5,77 +5,81 @@ with open("inputs/day5.txt") as f:
     true_input, *test_inputs = f.read().split("\n___INPUTSEP___\n")
 
 
-class PartOne:
+class Table:
+    def __init__(self, number_grid):
+        self._grid = number_grid
+        self.source_column = [row[1] for row in number_grid]
 
-    def __init__(self, raw_input):
-        seed_section, *map_sections = raw_input.split("\n\n")
-
-        self.seeds = [int(n) for n in re.findall(r"\d+", seed_section)]
-        self.maps = {}
-
-        for section in map_sections:
-            title_line, *map_lines = section.splitlines()
-            title = title_line.split()[0]
-            self.maps[title] = [[int(n) for n in line.split()] for line in map_lines]
-    
-    def convert(self, conversion_type, source):
-        for dest_start, source_start, length in self.maps[conversion_type]:
+    def convert(self, source):
+        for dest_start, source_start, length in self._grid:
             diff = source - source_start
             if 0 <= diff < length:
                 return dest_start + diff
         return source
 
+    def convert_backward(self, dest):
+        for dest_start, source_start, length in self._grid:
+            diff = dest - dest_start
+            if 0 <= diff < length:
+                return source_start + diff
+        return dest
+
+
+class PartOne:
+
+    def __init__(self, raw_input):
+        seed_section, *table_sections = raw_input.split("\n\n")
+
+        self.seeds = [int(n) for n in re.findall(r"\d+", seed_section)]
+        self.tables = []
+
+        for section in table_sections:
+            title_line, *table_strings = section.splitlines()
+            number_grid = [[int(n) for n in line.split()] for line in table_strings]
+            self.tables.append(Table(number_grid))
+
+    def go_downstream(self, number, table_index=0):
+        for table in self.tables[table_index:]:
+            number = table.convert(number)
+        return number
 
     def main(self):
         min_loc = float("inf")
 
         for seed in self.seeds:
-            number = seed
-            for conversion_type in self.maps:
-                number = self.convert(conversion_type, number)
-            if number < min_loc:
-                min_loc = number
+            location = self.go_downstream(seed)
+            if location < min_loc:
+                min_loc = location
         return min_loc
 
 
 class PartTwo(PartOne):
 
-    def convert_backward(self, conversion_type, source):
-        for source_start, dest_start, length in self.maps[conversion_type]:
-            diff = source - source_start
-            if 0 <= diff < length:
-                return dest_start + diff
-        return source
-
     def has_seed(self, seed):
-        for start, length in zip(self.seeds[::2], self.seeds[1::2]):
+        number_pairs = zip(self.seeds[::2], self.seeds[1::2])
+        for start, length in number_pairs:
             if start <= seed < start + length:
                 return True
         return False
 
-    def go_downstream(self, map_index, number):
-        conversion_types = list(self.maps.keys())
-        for type in conversion_types[map_index:]:
-            number = self.convert(type, number)
-        return number
-
-    def go_upstream(self, map_index, number):
-        conversion_types = list(self.maps.keys())
-        for type in reversed(conversion_types[:map_index]):
-            number = self.convert_backward(type, number)
+    def go_upstream(self, number, table_index):
+        for table in reversed(self.tables[:table_index]):
+            number = table.convert_backward(number)
         return number
 
     def main(self):
-        min_loc = PartOne(true_input).main()
+        min_loc = float("inf")
 
-        maps = list(self.maps.values())
-        for index, map in enumerate(maps):
-            for _, source, _ in map:
-                loc = self.go_downstream(index, source)
-                if loc < min_loc:
-                    seed = self.go_upstream(index, source)
-                    if self.has_seed(seed):
-                        min_loc = loc
+        for i, table in enumerate(self.tables):
+            boundaries = table.source_column
+            for number in boundaries:
+                location = self.go_downstream(number, i)
+                if location >= min_loc:
+                    continue
+                seed = self.go_upstream(number, i)
+                if self.has_seed(seed):
+                    min_loc = location
+
         return min_loc
 
 
@@ -92,8 +96,6 @@ def test_input_is_parsed_correctly():
     expected_soil = [81, 14, 57, 13]
     for seed, soil in zip(sol.seeds, expected_soil):
         assert sol.convert("seed-to-soil", seed) == soil
-
-
 
 def test_part1_gives_correct_result_on_test_input():
     assert PartOne(test_inputs[0]).main() == 35
