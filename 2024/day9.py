6a89e2d5979bd5f56790b48d443a074fed99c92f
Ragnar Gerholm <ragnar.gerholm@gmail.com>
2024-12-09 07:10:34 +0100

    add 2024 day 9

---
+++ 2024/day9.py
@@ -0,0 +1,72 @@
+import argparse
+import itertools
+
+
+def parse(data):
+    files = [[i] * int(n) for i, n in enumerate(data[::2])]
+    gaps = [[0] * int(n) for n in data[1::2]]
+
+    return files, gaps
+
+
+def part1(data):
+    files, gaps = parse(data)
+
+    gap_i = 0
+
+    while gap_i < len(gaps):
+        gap = gaps[gap_i]
+
+        for i in range(len(gap)):
+            if not files[-1]:
+                files.pop()
+                gaps.pop()
+            gap[i] = files[-1].pop()
+        gap_i += 1
+    
+
+    all_digits = []
+    for file, gap in itertools.zip_longest(files, gaps, fillvalue=[]):
+        all_digits += file + gap
+
+    return sum(i * d for i, d in enumerate(all_digits))
+        
+
+def part2(data):
+    files, gaps = parse(data)
+
+    file_i = len(files) - 1
+    while file_i >= 0:
+        file = files[file_i]
+        for gap in gaps[:file_i]:
+
+            if gap.count(0) >= len(file):
+                first_0 = gap.index(0)
+                gap[first_0:first_0 + len(file)] = file
+                file[:] = [0] * len(file)
+                break
+
+        file_i -= 1
+
+    all_digits = []
+    for file, gap in itertools.zip_longest(files, gaps, fillvalue=[]):
+        all_digits += file + gap
+
+    return sum(i * d for i, d in enumerate(all_digits))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--test", action="store_true", help="Run with test data")
+    args = parser.parse_args()
+
+    if args.test:
+        data_path = "test_data/day9.txt"
+    else:
+        data_path = "inputs/day9.txt"
+
+    with open(data_path) as file:
+        data = file.read()
+
+    print(part1(data))
+    print(part2(data))
